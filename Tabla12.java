0a2151a6e138c85201aa5937c8ef4754d9681963
lorena <lorena@gmail.com>
2026-06-06 19:50:39 +0200

    Modificacion tabla 12 lorena

--- Tabla12.java
+++ Tabla12.java
@@ -1,7 +1,7 @@
 public class Tabla12 {
     public static void main(String args[]) {
-        for(int i = 0; i <= 10; i++) {
-            System.out.println("10 * "+ i + " = " + 10 * i);
+        for(int i = 0; i <= 30; i++) {
+            System.out.println("12 * "+ i + " = " + 12 * i);
         }
     }
 }
